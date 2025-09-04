68436452f2053fcdadafcd3e9ca42ecf8d899310
shri-at <shrivatsunat@gmail.com>
2025-09-04 19:12:04 +0530

    Add files via upload

---
+++ CNN training/losses.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
+
+def binary_cross_entropy(y_true, y_pred):
+    return np.mean(-y_true * np.log(y_pred) - (1 - y_true) * np.log(1 - y_pred))
+
+def binary_cross_entropy_prime(y_true, y_pred):
+    return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
+
+def categorical_cross_entropy(y_true, y_pred):
+    y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+    return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+
+def categorical_cross_entropy_prime(y_true, y_pred):
+    return -y_true / np.clip(y_pred, 1e-15, 1 - 1e-15)
